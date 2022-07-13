0ae302ac2588d6a7d440e494fe39236684549db7
adityawithdoublea <78084725+adityawithdoublea@users.noreply.github.com>
2022-07-13 17:09:15 +0530

    Update sum_app.py

--- sum_app.py
+++ sum_app.py
@@ -208,14 +208,14 @@ st.markdown("---")
 url_text = st.text_input("Enter/Paste a url below:")
 
 st.markdown(
-    "<h3 style='text-align: center; color: #a8a7a5;'>OR</h3>",
+    "<h5 style='text-align: center; color: ##bababa;'>OR</h5>",
     unsafe_allow_html=True,
 )
 
 plain_text = st.text_input("Enter/Paste raw text below:")
 
 st.markdown(
-    "<h3 style='text-align: center; color: #a8a7a5;'>OR</h3>",
+    "<h5 style='text-align: center; color: ##bababa;'>OR</h5>",
     unsafe_allow_html=True,
 )
 
